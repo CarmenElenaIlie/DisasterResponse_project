201acce1d62c60abf540454fba8cdc643a579298
CarmenElenaIlie <144029233+CarmenElenaIlie@users.noreply.github.com>
2023-10-09 12:12:56 +0300

    process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -77,7 +77,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('df', engine, index=False)
+    df.to_sql('df', engine, index=False, if_exists='replace')
     pass
 
 def main():
