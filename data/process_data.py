919417ed42c1714a618251efaa1688de50d8903f
CarmenElenaIlie <144029233+CarmenElenaIlie@users.noreply.github.com>
2023-10-09 13:36:58 +0300

    process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,8 @@
+# Load Libraries
 import sys
-import nltk
-import re
-import numpy as np
 import pandas as pd
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sqlalchemy import create_engine  
+import  numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
@@ -31,59 +22,80 @@ def load_data(messages_filepath, categories_filepath):
     """
     # load messages dataset
     messages = pd.read_csv(messages_filepath)
-   
-    # load categories dataset
     categories = pd.read_csv(categories_filepath)
-    
     # merge datasets
-    df = pd. merge(messages, categories)
-    
-    # create a dataframe of the 36 individual category columns
-    categories = df['categories'].str.split(pat=';',expand=True)
-    
-    # select the first row of the categories dataframe
-    row = categories.iloc[0]
-    
-    # use this row to extract a list of new column names for categories.
-    category_colnames = row.apply(lambda x:x.rstrip('- 0 1'))
-   
-    # rename the columns of `categories`
-    categories.columns = category_colnames
-   
-    #Convert category values to just numbers
-    for column in categories:
-        # set each value to be the last character of the string
-        categories[column] = categories[column].str[-1]
-        # convert column from string to numeric
-        categories[column] = pd.to_numeric(categories[column])
+    df = messages.merge(categories, how='inner', on='id')
 
-    # drop the original categories column from `df`
-    df.drop('categories',axis=1,inplace=True)
-    
-    # concatenate the original dataframe with the new `categories` dataframe
-    df = pd.concat([df,categories],axis=1,sort=False)
-
-	    # remove all rows where related equals 2
-    df = df[df.related != 2]
     return df
 
 def clean_data(df):
-    # check number of duplicates
-    print("Number of duplicate rows in df: ",df.duplicated().sum())
-    
-    # drop duplicates
-    df=df.drop_duplicates()
-    return df
+    """Function to clean the Pandas DataFrame created from the load_data function
 
-  
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        Messages and Categories Pandas DataFrame
+    -------
+    Pandas DataFrame
+        Returns a clean Pandas DataFrame
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use the row to extract a list of new column names for categories
+    category_colnames = list(row.apply(lambda col: col.split('-')[0]))
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-').str[1]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int64')
+
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis='columns')
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    # filter out null values in related columns
+    df = df[pd.notnull(df.related)]
+
+    # remove all rows where related equals 2
+    df = df[df.related != 2]
+    return df
 
 
 def save_data(df, database_filename):
-    engine = create_engine('sqlite:///'+ database_filename)
+    """Function to save the clean dataframe to sqlite database
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        clean Pandas DataFrame
+	database_filename: str
+        Path to sqlite database destination file
+
+    Returns
+    -------
+    None
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('df', engine, index=False, if_exists='replace')
-    pass
 
 def main():
+    """Main function to run the ETL pipeline
+
+    Functions:
+    1) Extraction -> extract messages and categories from csv files
+	2) Transformation -> clean and pre-process data
+	3) Load -> load the clean dataframe to SQLite local database
+	"""
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
@@ -94,18 +106,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
               'DisasterResponse.db')
 
 
